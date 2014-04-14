c74966caa38d0949813002efc818675e53b46cd0
Alexis <asmalinowski@users.noreply.github.com>
2014-04-14 15:27:10 -0500

    Create BallTest.java

---
+++ BallTest.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+class BallTest
+{
+    public static void main(String[] args) 
+    {
+        BouncingBall app = new BouncingBall();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+    }
+}
